391f0b4aab79b478914baddf5f854b4384b4d454
Anant1606 <anantsharma.1839@gmail.com>
2024-08-31 19:28:37 +0530

    commit

--- server/routes/user.routes.cjs
+++ server/routes/user.routes.cjs
@@ -1,4 +1,6 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const { isAuthenticatedUser, isAdmin, isBlocked } = require('../middleware/app.authentication.cjs');
 const {
   getUser,
